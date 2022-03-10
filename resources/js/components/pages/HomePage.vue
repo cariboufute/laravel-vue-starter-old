198c07ed7f8a3bc94f66ba22f0b4eab512b5107a
CaribouFuté <frederic.chiasson@gmail.com>
2022-03-09 23:33:30 -0500

    Added login form

--- resources/js/components/pages/HomePage.vue
+++ resources/js/components/pages/HomePage.vue
@@ -2,10 +2,18 @@
     <h2>Home Page</h2>
 
     <p>You need authentication to access this page.</p>
+
+    <button @click="logout">Logout</button>
 </template>
 
 <script>
-    export default {};
+    export default {
+        methods: {
+            logout() {
+                alert('logout');
+            }
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
--- resources/js/components/pages/IndexPage.vue
+++ resources/js/components/pages/IndexPage.vue
@@ -2,12 +2,49 @@
     <h2>Index Page</h2>
 
     <p>Here is a page that does not need any authentication to access.</p>
+
+    <form @submit.prevent="login">
+        <div class="input-div">
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" v-model="form.email">
+        </div>
+
+        <div class="input-div">
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" v-model="form.password">
+        </div>
+        
+        <div class="input-div">
+            <button type="submit">Login</button>
+        </div>
+    </form>
 </template>
 
 <script>
-    export default {};
+    export default {
+        data() {
+            return {
+                form: {
+                    email: '',
+                    password: '',
+                }
+            };
+        },
+
+        methods: {
+            login() {
+                alert('login');
+            }
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
+    .input-div {
+        margin-bottom: 2rem;
 
+        label {
+            display: block;
+        }
+    }
 </style>
